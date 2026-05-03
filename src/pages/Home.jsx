1f2d1a06f5275dc21419984b393eeb9a34dcc606
Sebastian Resek <sebaresekk@gmail.com>
2026-05-03 19:43:09 -0300

    whatsapp support

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Calculator from '@/components/Calculator';
 import Features from '@/components/Features';
 import Testimonials from '@/components/Testimonials';
 import Requirements from '@/components/Requirements';
+import ButtonSupport from '@/components/ButtonSupport';
 import FAQ from '@/components/FAQ';
 
 const Home = () => (
@@ -14,7 +15,7 @@ const Home = () => (
          <div className="absolute -top-24 -left-24 w-96 h-96 bg-blue-100 rounded-full blur-3xl opacity-50"></div>
          <div className="absolute top-1/2 -right-24 w-125 h-125 bg-green-50 rounded-full blur-3xl opacity-40"></div>
       </div>
-       
+      <ButtonSupport />
       <Calculator />
       <Features />
       <Requirements />
